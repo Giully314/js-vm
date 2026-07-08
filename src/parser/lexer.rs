b3a6eba963e05c29f9bc95422701fe016f70c07e
Giully314 <giully@protonmail.com>
2026-07-09 00:00:23 +0200

    feat: complete token display.

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,6 +1,4 @@
-use std::{iter::Peekable, str::Chars, iter::Iterator};
-
-
+use std::{iter::Peekable, str::Chars, iter::Iterator, fmt::Display};
 
 enum Token {
     Keyword(Keyword),
@@ -8,27 +6,32 @@ enum Token {
     Number(String),
     String(String),
 
-    Dot,
-    Comma,
-    Semicolon,
-    OpenParen,
-    CloseParen,
-    OpenSquare,
-    CloseSquare,
-    OpenBracket,
-    CloseBracket,
+    Dot,            // .
+    Comma,          // ,
+    Semicolon,      // ;
+    OpenParen,      // ( 
+    CloseParen,     // )
+    OpenSquare,     // [
+    CloseSquare,    // ] 
+    OpenBracket,    // {
+    CloseBracket,   // }
 
-    Equal,
-    NotEqual,
-    GreaterThan,
-    GreaterThanOrEqual,
-    LessThan,
-    LessThanOrEqual,
-    Plus,
-    Minus,
-    Slash,
-    Star,
-    Percent,
+
+    Bang,                   // !
+    Equal,                  // =
+    Equality,               // ==
+    StrictEquality,         // ===
+    Inequality,             // !=
+    StrictInequality,       // !==
+    GreaterThan,            // >
+    GreaterThanOrEqual,     // >=
+    LessThan,               // <
+    LessThanOrEqual,        // <=
+    Plus,                   // +
+    Minus,                  // -
+    Slash,                  // /
+    Star,                   // *
+    Percent,                // %
 
     True,
     False,
@@ -36,6 +39,42 @@ enum Token {
     Null,
 }
 
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Self::Keyword(k) => return k.fmt(f),
+            Self::Identifier(s) => s,
+            Self::Number(n) => n,
+            Self::String(s) => s,
+            Self::Dot => ".",
+            Self::Comma => ",",
+            Self::Semicolon => ";",
+            Self::OpenParen => "(",
+            Self::CloseParen => ")",
+            Self::OpenSquare => "[",
+            Self::CloseSquare => "]",
+            Self::OpenBracket => "{",
+            Self::CloseBracket => "}" ,
+            Self::Equal => "=" ,
+            Self::Equality => "==" ,
+            Self::StrictEquality => "===" ,
+            Self::Inequality => "!=" ,
+            Self::StrictInequality => "!==" ,
+            Self::GreaterThan => "" ,
+            Self::GreaterThanOrEqual => "" ,
+            Self::LessThan => "" ,
+            Self::LessThanOrEqual => "" ,
+            Self::Plus => "" ,
+            Self::Minus => "" ,
+            Self::Slash => "" ,
+            Self::Star => "" ,
+            Self::Percent => "" ,
+            Self::True => "" ,
+            Self::False => "" ,
+            Self::Null => "" ,
+        })
+    }
+}
 
 enum Keyword {
     Await,
@@ -74,6 +113,49 @@ enum Keyword {
     Void,
     While,
     With,
+}
+
+impl Display for Keyword {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Self::Await => "await",
+            Self::Break => "break",
+            Self::Case => "case",
+            Self::Catch => "catch",
+            Self::Class => "class",
+            Self::Const => "const",
+            Self::Continue => "continue",
+            Self::Debugger => "debugger",
+            Self::Default => "default",
+            Self::Delete => "delete",
+            Self::Do => "do",
+            Self::Else => "else",
+            Self::Export => "export",
+            Self::Extends => "extends",
+            Self::False => "false",
+            Self::Finally => "finally",
+            Self::For => "for",
+            Self::Function => "function",
+            Self::If => "if",
+            Self::Import => "import",
+            Self::In => "in",
+            Self::Instanceof => "instanceof",
+            Self::New => "new",
+            Self::Null => "null",
+            Self::Return => "return",
+            Self::Super => "super",
+            Self::Switch => "switch",
+            Self::This => "this",
+            Self::Throw => "throw",
+            Self::True => "true",
+            Self::Try => "try",
+            Self::Typeof => "typeof",
+            Self::Var => "var",
+            Self::Void => "void",
+            Self::While => "while",
+            Self::With => "with",
+        })   
+    }
 }
 
 struct Lexer<'a> {
